4995c5179e8af69bcd648d0e543821620a9fc9fb
ELCOT <ELCOT@Lenovo>
2024-08-20 10:35:00 +0530

    commit

---
+++ C43_RUBIKA/src/com/tnsif/day2/KeyPrimitive/Primitive.java
@@ -0,0 +1,12 @@
+package com.tnsif.day2.KeyPrimitive;
+
+public class Primitive {
+
+	public static void main(String[] args) {
+		int a=3;
+		int b=4;
+		int c=a*b;
+		System.out.println(c);
+	}
+
+}
